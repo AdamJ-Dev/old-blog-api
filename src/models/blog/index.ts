484d3f43b47466fbc22e258e145e2d851c384de4
AdamJ-Dev <adam.g.johnson@hotmail.co.uk>
2022-09-12 17:20:29 +0100

    feat: blog model

---
+++ src/models/blog/index.ts
@@ -0,0 +1,30 @@
+import mongoose, { Date, Schema, Types } from "mongoose";
+
+export type BlogKeys = {
+  _id: Types.ObjectId;
+  title: string;
+  path: string;
+  body: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+const BlogSchema: Schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  path: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+const Blog = mongoose.model<BlogKeys>("Blog", BlogSchema);
+export default Blog;
