268d139f2755ca1c4abddcb2dbbeecae3d7f8e23
AdamJ-Dev <adam.g.johnson@hotmail.co.uk>
2022-09-14 17:37:51 +0100

    refactor: findUser returns whole user

--- src/controllers/auth/find-user/index.ts
+++ src/controllers/auth/find-user/index.ts
@@ -7,9 +7,8 @@ const findUser = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     const user = await User.findById(id);
-    if (!user) {
-      throw Error(NO_SUCH_USER)
-    } else res.status(200).json({ id });
+    if (!user) throw Error(NO_SUCH_USER);
+    else res.status(200).json({ user });
   } catch (err) {
     res.status(404).json({ error: getErrorMessage(err) });
   }
