d30cf005e22a7ccb0ffd1efc4faf2e7c8af6e279
AdamJ-Dev <adam.g.johnson@hotmail.co.uk>
2022-09-15 21:22:53 +0100

    fix: read 'id' from params (updateComments)

--- src/controllers/comments/put/index.ts
+++ src/controllers/comments/put/index.ts
@@ -4,10 +4,10 @@ import { getErrorMessage } from "../../../utility/get-error-message";
 import Comment from "../../../models/comment";
 
 const updateComment = async (req: Request, res: Response) => {
-  const { comment_id } = req.params;
+  const { id } = req.params;
   const { pinned } = req.body;
   try {    
-    const comment = await Comment.findById(comment_id);
+    const comment = await Comment.findById(id);
     if (!comment) throw Error(NO_SUCH_COMMENT);
     
     Object.assign(comment, { pinned });
