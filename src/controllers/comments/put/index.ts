fb66f8750e7bb954a783278da12b5fdd5fd75bc4
AdamJ-Dev <adam.g.johnson@hotmail.co.uk>
2022-09-15 21:21:46 +0100

    fix: remove unnecessary check that pinned is defined

--- src/controllers/comments/put/index.ts
+++ src/controllers/comments/put/index.ts
@@ -1,14 +1,12 @@
 import type { Request, Response } from "express";
-import { NO_SUCH_COMMENT, NO_UPDATES_PROVIDED } from "../../../constants/errors";
+import { NO_SUCH_COMMENT } from "../../../constants/errors";
 import { getErrorMessage } from "../../../utility/get-error-message";
 import Comment from "../../../models/comment";
 
 const updateComment = async (req: Request, res: Response) => {
   const { comment_id } = req.params;
   const { pinned } = req.body;
-  try {
-    if (pinned === undefined) throw Error(NO_UPDATES_PROVIDED);
-    
+  try {    
     const comment = await Comment.findById(comment_id);
     if (!comment) throw Error(NO_SUCH_COMMENT);
     
