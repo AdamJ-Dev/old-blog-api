c4a3402a3d0bb663535173be5733c408451570b7
AdamJ-Dev <adam.g.johnson@hotmail.co.uk>
2022-09-29 09:55:40 +0100

    feat: CORS policy

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,22 @@ import connectToMongoDb from "./utility/mongo-db/connect-to-mongo-db";
 import authRouter from "./routes/auth-routes";
 import blogRouter from "./routes/blog-routes";
 import commentsRouter from "./routes/comments-routes";
+import serve404 from "./controllers/404";
+import cors from "cors";
 
 import dotenv from "dotenv";
-import serve404 from "./controllers/404";
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 const app: Express = express();
+
+// CORS policy
+const CORS_ORIGINS = [process.env.UI_URL, process.env.CMS_URL] as string[];
+app.use(
+  cors({
+    origin: CORS_ORIGINS,
+  })
+);
 
 // Middleware to parse POST/PUT requests
 app.use(express.json());
