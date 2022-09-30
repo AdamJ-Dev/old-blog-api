78c2259d221222a417b3313697a89171a80faff9
AdamJ-Dev <adam.g.johnson@hotmail.co.uk>
2022-09-30 14:20:13 +0100

    refactor: client side auth cookie

--- src/controllers/auth/signup/index.ts
+++ src/controllers/auth/signup/index.ts
@@ -1,6 +1,5 @@
 import { getErrorMessage } from "../../../utility/get-error-message";
 import { createToken } from "../../../utility/auth/jwt";
-import { COOKIE_OPTIONS } from "../../../constants/cookie-options";
 import type { Request, Response } from "express";
 import User from "../../../models/user";
 
@@ -12,7 +11,6 @@ const signup = async (req: Request, res: Response) => {
     const token = createToken(id);
     const cookieData: Record<string, string> = { id, token };
     if (user.admin) cookieData['admin'] = user.admin; 
-    res.cookie("user", JSON.stringify(cookieData), COOKIE_OPTIONS);
     res.status(200).json({ user });
   } catch (err) {
     res.status(400).json({ error: getErrorMessage(err) });
